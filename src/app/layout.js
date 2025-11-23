ca8d7ac1f53ed7a6a8d101aaa9cafde20bcfea65
Isadora Chieregato <chieregatoisadora@gmail.com>
2025-11-23 11:14:21 -0300

    Inicio do desenvolvimento

---
+++ src/app/layout.js
@@ -0,0 +1,38 @@
+import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  display: 'swap',
+});
+
+
+export const metadata = {
+  title: "AgenteIA - Seu assistente de IA pessoal",
+  description:
+    " AgenteIA é um assistente de IA pessoal que ajuda você a gerenciar tarefas, responder perguntas e melhorar sua produtividade com inteligência artificial avançada.",
+
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to AgenteIA</h1>
+      <p>Your personal AI assistant to help manage tasks, answer questions, and boost productivity with advanced artificial intelligence.</p>
+      
+      
+    </div>
+    
+  );
+}
